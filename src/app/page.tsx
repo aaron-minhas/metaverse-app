140e68cec70cea8ce37a8dcb3d3cdaa41280f965
aaron-minhas <133404313+aaron-minhas@users.noreply.github.com>
2024-06-07 09:51:04 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,41 @@
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import About from "@/sections/About";
+import Explore from "@/sections/Explore";
+import Feedback from "@/sections/Feedback";
+import GetStarted from "@/sections/GetStarted";
+import Hero from "@/sections/Hero";
+import Insights from "@/sections/Insights";
+import WhatsNew from "@/sections/WhatsNew";
+import World from "@/sections/World";
+
+
+
+export default function Home() {
+  return (
+    <div className="bg-primary-black overflow-hidden">
+      <Navbar />
+        <div className="relative">
+          <Hero />
+            <div className="gradient-02" />
+        </div>
+        <div className="relative">
+          <About />
+            <div className="gradient-03 z-0" />
+          <Explore />
+        </div>
+        <div className="relative">
+          <GetStarted />
+            <div className="gradient-04 z-0" />
+          <WhatsNew />
+        </div>
+          <World />
+        <div className="relative">
+          <Insights />
+            <div className="gradient-04 z-0"/>
+          <Feedback />
+        </div>
+      <Footer />
+    </div>
+  )
+}
